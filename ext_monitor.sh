bcb44438a0a8b90ce11fafca8db6920585a3d628
ThienJP <nhj@gmx.net>
2018-04-06 23:11:49 +0200

    quit lgs first

--- ext_monitor.sh
+++ ext_monitor.sh
@@ -1,9 +1,13 @@
 # this script checks if my bluetooth mouse is connected and if so starts my external monitor setup and turn off wifi
 
-
-if [ $(system_profiler -detaillevel basic SPBluetoothDataType | pcregrep -M  'Logitech MX900.*(\n|.)*0x2201' | grep 'Connected: Yes' | wc -l) -eq 1 ]; then
+if [ $(system_profiler -detaillevel basic SPBluetoothDataType | pcregrep -M  'Logitech MX900.*(\n|.)*0x2, 0x545' | grep 'Connected: Yes' | wc -l) -eq 1 ]; then
     echo "Bluetooth Mouse connected"
     echo "starting external monitor setup..."
+
+    # since I can't seem to prevent LGS from auto starting I'll quit LGS first 
+    if [[ ! $(ps axo pid,command | grep "[L]ogitech\ Gaming\ Software.app") ]]; then
+      osascript -e 'quit app "Logitech Gaming Software"'
+    fi
 
     # check if smooze and lgs are running
     if [[ ! $(ps axo pid,command | grep "[S]mooze.app") ]] && [[ ! $(ps axo pid,command | grep "[L]ogitech\ Gaming\ Software.app") ]]; then
@@ -17,7 +21,7 @@ if [ $(system_profiler -detaillevel basic SPBluetoothDataType | pcregrep -M  'Lo
       echo "Smooze and LGS are already running!"
     fi
 
-    # check if wifi running and lan active
+    # check if both wifi running and lan active
     if [[ $(networksetup -getairportpower en0 | grep "On") ]] && [[ $(ifconfig en5 | grep "active") ]]; then
         networksetup -setairportpower en0 off
         echo "LAN active... deactivating WiFi."
